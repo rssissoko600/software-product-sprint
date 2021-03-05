aa6546083a67581e45e9ad23de581ed882905ccb
Rokiatou <rssissoko600@gmail.com>
2021-03-05 11:13:18 +0000

    Here'smy first ever servlet.

---
+++ portfolio/src/main/java/com/google/sps/servlets/HomeServlet.java
@@ -0,0 +1,19 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Handles requests sent to the /index URL. Try running a server and navigating to /index! 
+* Writes a hard-coded string as the response when a user requests its URL. */
+@WebServlet("/index")
+public class HomeServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("text/html;");
+    response.getWriter().println("<h1>Hello world!</h1>");
+  }
+}
